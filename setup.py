e84fecc7758b9d2b72df2d807228d03b2a5dd45a
Harley Bussell <harley@harley-desktop.(none)>
2010-01-06 12:08:40 +1100

    update setup

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from setuptools import setup, find_packages
-import os
 
 setup(name='django-slimmer',
 version='0.0.1',
 description='Html compression as middleware and view decorators',
 author='Harley Bussell',
 author_email='modmac@gmail.com',
+url='http://github.com/hbussell/django-slimmer',
+classifiers=[
+  "Programming Language :: Python",
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Developers",
+  "Framework :: Django",
+],
+keywords='django html compressor',
 zip_safe=False,
+license='MIT',
 install_requires=[
 'setuptools',
 ],
 packages = find_packages(),
 include_package_data = True,
 )
-
